da654b35fb20b81a2ebf1bbec50c2a8446986d45
baba_s <shotababa1206@gmail.com>
2020-10-04 11:39:48 +0900

    first commit

---
+++ Scripts/DirectoryUtils.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using System.IO;
+
+namespace Kogane
+{
+	public static class DirectoryUtils
+	{
+		[CanBeNull]
+		public static DirectoryInfo CreateDirectoryFromFilePath( [NotNull] string filePath )
+		{
+			var path = Path.GetDirectoryName( filePath );
+			if ( string.IsNullOrWhiteSpace( path ) ) return null;
+			return Directory.CreateDirectory( path );
+		}
+	}
+}
